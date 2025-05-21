0ebede9926a3753aaf4b0e1adebcb00be5c13888
Ryan B. Jiang <thewaitwhatog+github@gmail.com>
2025-05-21 19:32:50 -0400

    First commit

---
+++ hw_5_20_25.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <list>
+
+/*
+ * TODO: What's Happening
+ * I want you to research std::list or C++ standard library's linked list implementaiton.
+ * The problem to sove:
+ * you have intergers stored in vector<uint16_t> data
+ * I need you to put all of it into a linked list (std::list) as "first_version"
+ * Then, create a copy of this list in "second_version" 
+ * Without using std::list::reverse() or the list reverse function, reverse all values of second_version
+ * Finally, check that you did the reverse properly by using std::list::reverse() on first_version
+ * Verify.
+ *
+ * command to compile and run:
+ * gcc <name of file here> -Wall -Werror -o <name of program> && ./<name of program>
+ * && makes it so it will run the first command (gcc) before ./ or the run command
+ *
+ * All you should need to know is in cppreference; you can also search std::list
+ *
+ * 
+ *
+ *     
+ * 
+ */
+
+int main(int argc, char * argv[])
+{
+  
+  std::uint16_t = size; 
+  std::cout << "What is the size?: " << "\n";
+  std::cin >> size;
+
+  std::vector<uint16_t> data;
+  for (uint16_t i = 0; i < size; i++)
+  {
+    data.push_back(i);
+  }
+
+  
+
+
+
+  return 0;
+}
+
+
+
